0fa7fb82e7a31b969a9d42fde948c9c368a67420
vigneshrajacyces <vigneshraja108@gmail.com>
2023-03-28 16:25:50 +0530

    bug

--- hotel/testApp/urls.py
+++ hotel/testApp/urls.py
@@ -3,5 +3,4 @@ from .views import available
 
 urlpatterns = [
     path('rooms/', available, name="index"),
-    # path('rooms/?check_in=<date>&check_out=<date>&building=,building_name>', available, name="available"),
 ]
